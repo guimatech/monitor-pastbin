d2b2797e2ba88c3fc17ae34ed1c9db762af568ce
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2018-11-10 18:16:59 -0200

    script.sh criando arquivo temporario para filtrar novos links

--- script.sh
+++ script.sh
@@ -1,14 +1,18 @@
-#!bin/bash
+#!/bin/bash
 
-# * MONITORAR PASTEBIN E OBTER O LINK DAS POSTAGENS COM ALGUM CONTEUDO ESPECIFICO *
+touch templinks
 
 # ACESSAR O PASTEBIN
+links="$(curl -s "https://pastebin.com/archive" | grep "i_p0" | cut -d"=" -f5 | cut -d'"' -f2 | tr -d "/")"
 
 #   EXTRAIR OS LINKS DAS NOVAS POSTAGENS
+for l in $links; do
+  r=grep "$l" templinks
+  if [ "$r" == "" ]; then echo $l >> templinks; fi
+done
 
 #   ACESSAR CADA UM DOS LINKS E OBTER CONTEUDO
 
 #     FILTRAR O CONTEUDO QUE EU QUERO
 
 #     EXIBIR O LINK (OU NAO) / SALVAR O LINK
-
